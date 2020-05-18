62f2b3c68cbf8b75560d687f003f8a757e703179
mkovarky <mikhail.kovarsky@gmail.com>
2020-05-18 18:12:30 +0300

    fix selectors

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -6,8 +6,8 @@ import ru.netology.data.DataHelper;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-    private SelenideElement loginField = $("input[name=login]");
-    private SelenideElement passwordField = $("input[name=password]");
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
     private SelenideElement loginButton = $("[data-test-id=action-login]");
 
     public VerificationPage validLogin(DataHelper.AuthInfo info) {
